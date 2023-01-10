117efcdf5a7b9e4834029b8f89ddce49287c9eaf
JParkBro <hyeongjoopark0324@gmail.com>
2023-01-10 10:57:41 +0900

    Android controller 생성

--- src/main/java/com/clever/controller/AndroidController.java
+++ src/main/java/com/clever/controller/AndroidController.java
@@ -1,20 +1,17 @@
-package com.clever.androidtest;
-
-
+package com.clever.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Hello {
-	
-	@GetMapping("/hello/android")
-	public String join() {
-		
+public class AndroidController {
+
+	@GetMapping("/android/hello")
+	public String hello() {
+
 		System.out.println("Hello Android");
 
 		return "Hello Android";
 	}
-	
-	
+
 }
