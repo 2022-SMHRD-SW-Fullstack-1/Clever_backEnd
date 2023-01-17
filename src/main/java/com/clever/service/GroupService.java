73cc34f538083e35dd69265669413bfa01303f82
ahaelim <limahae1015@gmail.com>
2023-01-17 09:28:59 +0900

    시리얼번호생성 서비스로 이

--- src/main/java/com/clever/service/GroupService.java
+++ src/main/java/com/clever/service/GroupService.java
@@ -13,7 +13,27 @@ public class GroupService {
 	@Autowired
 	GroupMapper groupMapper;
 	public int addGroup(Group group_info) {
-
+		group_info.setGroup_serial(serialNum());
 		return groupMapper.addGroup(group_info);
 	}
+	public String serialNum() {
+		
+		int serialSize = 1;
+		final char[] possibleChar = {
+				'1','2','3','4','5','6','7','8','9','0','A','B','C','D','E','F',
+			     'G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V',
+			     'W','X','Y','Z'
+		};
+		final int possibleCharCnt = possibleChar.length;
+		String[] arr = new String[serialSize];
+		Random rd = new Random();
+		int currentIndex = 0;
+		int i = 0;
+		StringBuffer buf = new StringBuffer(16);
+		for(i=8 ; i>0 ; i--) {
+			buf.append(possibleChar[rd.nextInt(possibleCharCnt)]);
+		}
+		String group_serial = buf.toString();
+		return group_serial;
+}
 }
